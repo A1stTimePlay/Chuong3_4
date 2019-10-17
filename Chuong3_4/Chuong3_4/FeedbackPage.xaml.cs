2d266b1f4067bc23bae2c59eb59560e22ad31d83
NguyenXuanAn <nguyenxuanan.nxa@gmail.com>
2019-10-17 23:40:51 +0700

    Add project files.

---
+++ Chuong3_4/Chuong3_4/FeedbackPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Chuong3_4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FeedbackPage : ContentPage
+    {
+        const string placeHolderText = "Type your message here";
+        void HandleFeedback(object sender, Xamarin.Forms.FocusEventArgs e)
+        {
+            var text = Feedback.Text;
+            if (Feedback.Text == placeHolderText)
+            {
+                Feedback.Text = string.Empty;
+                return;
+            }
+            if (Feedback.Text == string.Empty)
+            {
+                Feedback.Text = placeHolderText;
+                return;
+            }
+        }
+        public FeedbackPage()
+        {
+            InitializeComponent();
+            Feedback.Text = placeHolderText;
+        }
+    }
+}
---
+++ Chuong3_4/Chuong3_4/StaticResourcePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Chuong3_4
+{
+    public partial class StaticResourcesPage : ContentPage
+    {
+        void HandleFeedback(object sender, Xamarin.Forms.FocusEventArgs e)
+        {
+            var placeHolderText = (string)Resources["FeedbackPlaceholder"];
+            if (Feedback.Text == placeHolderText)
+            {
+                Feedback.Text = string.Empty;
+                Feedback.TextColor = (Color)Resources["InputTextColor"];
+                return;
+            }
+            if (Feedback.Text == string.Empty)
+            {
+                Feedback.Text = placeHolderText;
+                Feedback.TextColor = (Color)Resources["InputPlaceholder
+               Color"];
+            return;
+            }
+        }
+    }
+}
